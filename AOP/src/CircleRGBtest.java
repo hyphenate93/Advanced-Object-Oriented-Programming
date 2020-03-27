45fb982442c637eeccb7bdec71388ee87a07a756
hyphenate93 <50260177+hyphenate93@users.noreply.github.com>
2020-03-27 11:18:53 +0100

    Update CircleRGBtest.java

--- AOP/src/CircleRGBtest.java
+++ AOP/src/CircleRGBtest.java
@@ -1,67 +1,72 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.util.Arrays;
 
 
-public class CircleRGBtest
-{
 
-	private static CircleRGB icon;
-    private static JLabel label;
-    private static Color color;
-
-    public static void main(String[] args)
-    {
-    	JFrame frame = new JFrame();
-      	JButton red = new JButton("Red");
-
-      	red.addActionListener(
-      			colorListener("red"));
-
-    	JButton blue = new JButton("Blue");
-
-    	blue.addActionListener(
-    			colorListener("blue"));
-
-    	JButton green = new JButton("Green");
-    	green.addActionListener(
-    			colorListener("green"));
-
-    	icon = new CircleRGB(200);
-    	label = new JLabel(icon);
-
-    	frame.setLayout(new FlowLayout());
-
-    	frame.add(red);
-    	frame.add(green);
-    	frame.add(blue);
-    	frame.add(label);
-
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      	frame.pack();
-      	frame.setVisible(true);
-    }
+	
+	import java.awt.*;
+	import java.awt.event.*;
+	import javax.swing.*;
 
 
-    public static ActionListener colorListener(final String button) {
-      return new
-         ActionListener()
-         {
-            public void actionPerformed(ActionEvent event)
-            {
-               if(button == "blue"){
-            	   color = Color.blue;
-               }
-               else if(button == "green"){
-            	   color = Color.green;
-               }
-               else if(button == "red"){
-            	   color = Color.red;
-               }
-               icon.setColor(color);
-               label.repaint();
-            }
-         };
-   }
+	public class CircleRGBtest
+	{
+		
+		private static CircleRGB icon;
+	    private static JLabel label;
 
-}
+	    
+	    public static void main(String[] args)
+	    {
+	    	JFrame frame = new JFrame();
+	      	JButton red = new JButton("Red");
+
+	      	red.addActionListener(
+	      			colorListener("red"));
+	      
+	    	JButton blue = new JButton("Blue");
+	    	
+	    	blue.addActionListener(
+	    			colorListener("blue"));      
+
+	    	JButton green = new JButton("Green");
+	    	green.addActionListener(
+	    			colorListener("green"));
+
+	    	icon = new CircleRGB(200);
+	    	label = new JLabel(icon);
+
+	    	frame.setLayout(new FlowLayout());
+	    	
+	    	frame.add(red);
+	    	frame.add(green);
+	    	frame.add(blue);
+	    	frame.add(label);
+
+	    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      	frame.pack();
+	      	frame.setVisible(true);
+	    }
+
+	    
+	    public static ActionListener colorListener(final String button) {
+	      return new
+	         ActionListener()
+	         {
+	            public void actionPerformed(ActionEvent event)
+	            {
+	               if(button == "blue"){
+	            	   icon.setColor(Color.blue);
+	               }
+	               else if(button == "green"){
+	            	   icon.setColor(Color.green);
+	               }
+	               else if(button == "red"){
+	            	   icon.setColor(Color.red);
+	               }
+	               label.repaint();
+	            }
+	         };
+	   }
+
+	}
+	
